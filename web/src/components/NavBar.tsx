d6c9f5e62f3b151a5a316b5e5f9bb23cf87bf74d
Shreyas <shreyas@Shreyass-MacBook-Pro.local>
2022-04-24 10:07:14 +0530

    cache-reset

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import NextLink from "next/link"
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const router = useRouter();
     const [{fetching: logoutFetching},logout] = useLogoutMutation()
     const [{ data, fetching }] = useMeQuery({
         pause: isServer(),
@@ -39,8 +41,9 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
                 <Box mr={2}>
                     {data.me.username}
                 </Box>
-                <Button onClick={() => {
-                    logout();
+                <Button onClick={async () => {
+                    await logout();
+                    router.reload()
                 }} 
                 isLoading={logoutFetching}
                 variant={"link"}>logout</Button>
--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -1,24 +1,23 @@
-import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
+import { Cache, cacheExchange, Resolver } from "@urql/exchange-graphcache";
+import Router from "next/router";
 import {
   dedupExchange,
   Exchange,
   fetchExchange,
   gql,
-  ssrExchange,
   stringifyVariables,
 } from "urql";
+import { pipe, tap } from "wonka";
 import {
-  LogoutMutation,
-  MeQuery,
-  MeDocument,
+  DeletePostMutationVariables,
   LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
   RegisterMutation,
   VoteMutationVariables,
-  DeletePostMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "../utils/betterUpdateQuery";
-import Router from "next/router";
-import { pipe, tap } from "wonka";
 import { isServer } from "./isServer";
 
 const errorExchange: Exchange =
@@ -71,59 +70,16 @@ const cursorPagination = (): Resolver => {
       hasMore,
       posts: results,
     };
-    //   const visited = new Set();
-    //   let result: NullArray<string> = [];
-    //   let prevOffset: number | null = null;
-
-    //   for (let i = 0; i < size; i++) {
-    //     const { fieldKey, arguments: args } = fieldInfos[i];
-    //     if (args === null || !compareArgs(fieldArgs, args)) {
-    //       continue;
-    //     }
-
-    //     const links = cache.resolve(entityKey, fieldKey) as string[];
-    //     const currentOffset = args[cursorArgument];
-
-    //     if (
-    //       links === null ||
-    //       links.length === 0 ||
-    //       typeof currentOffset !== "number"
-    //     ) {
-    //       continue;
-    //     }
-
-    //     const tempResult: NullArray<string> = [];
-
-    //     for (let j = 0; j < links.length; j++) {
-    //       const link = links[j];
-    //       if (visited.has(link)) continue;
-    //       tempResult.push(link);
-    //       visited.add(link);
-    //     }
-
-    //     if (
-    //       (!prevOffset || currentOffset > prevOffset) ===
-    //       (mergeMode === "after")
-    //     ) {
-    //       result = [...result, ...tempResult];
-    //     } else {
-    //       result = [...tempResult, ...result];
-    //     }
-
-    //     prevOffset = currentOffset;
-    //   }
-
-    //   const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
-    //   if (hasCurrentPage) {
-    //     return result;
-    //   } else if (!(info as any).store.schema) {
-    //     return undefined;
-    //   } else {
-    //     info.partial = true;
-    //     return result;
-    //   }
   };
 };
+
+function invalidateAllPosts(cache: Cache) {
+  const allFields = cache.inspectFields("Query");
+  const fieldInfos = allFields.filter((info) => info.fieldName === "posts");
+  fieldInfos.forEach((fi) => {
+    cache.invalidate("Query", "posts", fi.arguments || {});
+  });
+}
 
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
@@ -184,13 +140,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
               }
             },
             createPost: (_result, args, cache, info) => {
-              const allFields = cache.inspectFields("Query");
-              const fieldInfos = allFields.filter(
-                (info) => info.fieldName === "posts"
-              );
-              fieldInfos.forEach((fi) => {
-                cache.invalidate("Query", "posts", fi.arguments || {});
-              });
+              invalidateAllPosts(cache);
             },
             logout: (_result, args, cache, info) => {
               betterUpdateQuery<LogoutMutation, MeQuery>(
@@ -215,6 +165,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
                   }
                 }
               );
+              invalidateAllPosts(cache);
             },
             register: (_result, args, cache, info) => {
               betterUpdateQuery<RegisterMutation, MeQuery>(
